2192d7579e3848f07223ca0738eeddab98a40e26
vickynov13 <52153155+vickynov13@users.noreply.github.com>
2023-05-11 00:57:44 +0530

    Update trainServer_v1.js

--- trainServer_v1.js
+++ trainServer_v1.js
@@ -39,7 +39,7 @@ const bootHandler = async function (){
   keys=trainStat.add([
     {
       runStatus: 0,
-      speed: 0,
+      speed: 80,
       headLamp: 0,
       comp1light: 0,
       comp2light: 0,
@@ -75,6 +75,29 @@ app.get('/status', (req, res)=> {
         return res.send(trainStat.get(key));
 });
 
+app.get('/speed/:item', (req, res)=> {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  let tstat=trainStat.get(key);
+  if(tstat.runStatus == 1){
+    if(req.params.item == 'up'){
+      var upspd = tstat.speed + 20;
+      if(upspd >= 255){
+        tstat.speed = 255;
+      }else{tstat.speed = upspd;}
+    }else if(req.params.item == 'down'){
+      var lowspd = tstat.speed - 20;
+      if(lowspd <= 160){
+        tstat.speed = 160;
+      }else{tstat.speed = lowspd;}
+    }
+  }
+  trainStat.update(key, tstat);
+  return res.send(tstat);
+});
+
 app.get('/set/:item', (req, res)=> {
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -82,16 +105,18 @@ app.get('/set/:item', (req, res)=> {
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   let tstat=trainStat.get(key);
   if(req.params.item == 'speed'){
-    if(req.query.value >=0 && req.query.value <=255){
-      tstat.speed=req.query.value;
+    if(tstat.runStatus == 1){
+      if(req.query.value >=160 && req.query.value <=255){
+        tstat.speed=req.query.value;
+      }
     }
   }
   if(req.params.item == 'runStatus'){
     if(req.query.value == 0){
-      tstat.speed = 0;
+      tstat.speed = 100;
       tstat.runStatus = 0;
     }else if(req.query.value == 1){
-      tstat.speed = 50;
+      tstat.speed = 160;
       tstat.runStatus = 1;
     }
   }
